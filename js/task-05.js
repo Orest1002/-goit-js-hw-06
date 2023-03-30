edc49421be1482d6197e6cbf25b1147093907c44
Orest <Orest347@gmail.com>
2023-03-30 11:03:45 +0300

    tack 6

--- js/task-05.js
+++ js/task-05.js
@@ -5,5 +5,5 @@ const output = document.querySelector("#name-output");
 nameInput.addEventListener("input",(event) => {
     output.textContent = event.currentTarget.value;  
 });
-console.log( nameInput)
+
 
