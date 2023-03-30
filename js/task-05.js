fe25bb51342666b326bdefd799ab3bf41129c2b7
Orest <Orest347@gmail.com>
2023-03-30 17:56:26 +0300

    tasc5

--- js/task-05.js
+++ js/task-05.js
@@ -2,8 +2,16 @@
 const nameInput = document.querySelector("#name-input");
 const output = document.querySelector("#name-output");
 
-nameInput.addEventListener("input",(event) => {
-    output.textContent = event.currentTarget.value;  
-});
+nameInput.addEventListener("input",(event ) => {
+
+
+
+   ( output.textContent = event.currentTarget.value);  
+
+if (input.value === "" ) {
+     output.textContent ("Anonymous");
+  }
+}
+);
 
 
